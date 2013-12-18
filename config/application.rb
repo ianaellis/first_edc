df5e385fa414c09480f591d3b4af5a428710fcc1
Ian Ellis <ianaellis@gmail.com>
2013-12-18 11:01:22 -0800

    Moved a thing

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,11 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-config.assets.initialize_on_precompile = false
 
 module FirstEdc
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
