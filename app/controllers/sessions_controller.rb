56177becaf5802bda51bc75a2538c825ea681afb
Ian Ellis <ianaellis@gmail.com>
2014-05-02 11:41:47 -0700

    Log in routes to home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
     before_filter :authenticate_user!
 	def new
 		# user = User.find_by_email(params[:session][:email])
-		redirect_to subjects_url
+		redirect_to root_path
 	end
 
 	def create
 		user = User.find_by_email(params[:session][:email])
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_to @user
+			redirect_to root_path
 		else
 			flash.now[:error] = 'Invalid email/password combination'
 			render 'new'
